2a4154bbbcc5510b12ef69ed46556d9349cdd4b2
Le Tuan Anh <letuananh.programming@gmail.com>
2019-01-19 10:44:22 +0700

    Fix bugs

--- demo/src/main/java/com/flipkart/springyheads/demo/ChatHeadService.java
+++ demo/src/main/java/com/flipkart/springyheads/demo/ChatHeadService.java
@@ -19,7 +19,6 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import com.flipkart.chatheads.ui.ChatHead;
 import com.flipkart.chatheads.ui.ChatHeadArrangement;
-import com.flipkart.chatheads.ui.ChatHeadConfig;
 import com.flipkart.chatheads.ui.ChatHeadListener;
 import com.flipkart.chatheads.ui.ChatHeadViewAdapter;
 import com.flipkart.chatheads.ui.MaximizedArrangement;
@@ -216,8 +215,8 @@ public class ChatHeadService extends Service {
 
     public void addChatHead() {
         chatHeadIdentifier++;
-        chatHeadManager.addChatHead(String.valueOf(1), false, true);
-        chatHeadManager.bringToFront(chatHeadManager.findChatHeadByKey(String.valueOf(1)));
+        /*chatHeadManager.addChatHead(String.valueOf(1), false, true);
+        chatHeadManager.bringToFront(chatHeadManager.findChatHeadByKey(String.valueOf(1)));*/
     }
 
     public void removeChatHead() {
